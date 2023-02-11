0f7d9c852f4261680dc9b3670c4d9d75e95bbcb1
rgraner <ricardograner@gmail.com>
2023-02-11 17:36:14 +0000

    Fix subreddits link

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,6 @@ import { PostList } from '../components/PostList';
 import { Sidebar } from '../components/Sidebar';
 import '../index.css';
 
-
 export const App = () => {
   const [posts, setPosts] = useState([]);
   const [subreddits, setSubreddits] = useState([]);
@@ -15,24 +14,33 @@ export const App = () => {
       const res = await fetch(`https://www.reddit.com/r/${selectedSubreddit}.json`);
       const json = await res.json();
       setPosts(json.data.children);
-      setSubreddits(json.data.children);
     };
     fetchData();
   }, [selectedSubreddit]);
 
-  const handleSubredditSelection = (subreddit) => {
-    setSelectedSubreddit(subreddit);
+  useEffect(() => {
+    const fetchSubreddits = async () => {
+      const res = await fetch('https://www.reddit.com/r/popular.json');
+      const json = await res.json();
+      setSubreddits(json.data.children);
     };
+    fetchSubreddits();
+  }, []);
+
+  const onSubredditSelection = subreddit => {
+    setSelectedSubreddit(subreddit);
+  };
 
   return (
     <div className="app">
         <Header />
         <main className="main">
             <PostList posts={posts} />
-            <Sidebar subreddits={subreddits} onSubredditSelection={handleSubredditSelection}/>
+            <Sidebar subreddits={subreddits} onSubredditSelection={onSubredditSelection} />
         </main>
     </div>
-  )
-}
+  );
+};
+
 
 
