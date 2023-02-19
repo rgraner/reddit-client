027dbedd0d6efc0bce4bc539205f382a334394f5
rgraner <ricardograner@gmail.com>
2023-02-19 14:45:06 +0000

    Replace image logo for text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from '../images/reddit-c_logo.png';
 
 export const Header = ({ onSearch, onLogoClick }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -16,7 +15,7 @@ export const Header = ({ onSearch, onLogoClick }) => {
   return (
     <header>
       <div className='header-container'>
-        <img src={logo} alt="Logo" onClick={handleLogoClick} />
+        <div className="logo" onClick={handleLogoClick}>REDDIT<span>-C</span></div>
         <div className="header-right-side">
         <a href="https://github.com/rgraner/reddit-client" title="Github Reddit-Client"><i className="fa-brands fa-github"></i></a>
           <input type="text" placeholder="Search Reddit" value={searchTerm} onChange={handleSearch} />
